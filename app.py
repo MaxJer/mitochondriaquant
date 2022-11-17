2b65fa7182aadd9504785b327242750cf375848c
MaxJer <max.jergitsch@gmail.com>
2022-11-17 11:20:47 +0100

    updated

--- app.py
+++ app.py
@@ -70,9 +70,9 @@ ALLOWED_EXTENSIONS = ["png", "jpg", "jpeg"]
 st.subheader("Parameters")
 
 ## add a text
-st.markdown("## ROI 
+st.markdown("## ROI" 
 
-Roi is really cool")
+"Roi is really cool")
 
 ## add a button to upload an image either png or jpg
 uploaded_file = st.file_uploader("Upload your image", type=ALLOWED_EXTENSIONS)
